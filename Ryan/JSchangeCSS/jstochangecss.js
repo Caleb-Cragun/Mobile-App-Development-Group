fd64d2f2c91094180fd27c24fe99a2bb3195968f
Ryan Joos <ryanjoos@Ryans-MacBook-Pro-2.local>
2015-06-10 14:45:01 -0600

    JS change CSS stuff

---
+++ Ryan/JSchangeCSS/jstochangecss.js
@@ -0,0 +1,18 @@
+function makeBlue(){
+	document.getElementById("color").className = "blue";
+}
+
+function makeRed(){
+	document.getElementById("color").className = "red";
+}
+
+//SandBox Code
+
+function makeBlueBad(){
+	document.getElementById("element").className = "blue";
+}
+
+function makeRedBad(){
+	document.getElementById("color").className = red;
+}
+
